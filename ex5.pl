71655143a42ea2671fb45a75546ee4d804e5a371
liadatz <liad.atsmon@gmail.com>
2021-06-11 23:02:39 +0300

    update 23:02 11.6

--- ex5.pl
+++ ex5.pl
@@ -40,3 +40,39 @@ book("The Lord of the Rings", 4, 4, 1250).
 
 % You can add more facts.
 % Fill in the Purpose, Signature as requested in the instructions here
+
+% Signature: authorOfGenre(GenreName, AuthorName)/2
+% Purpose: Check if AuthorName wrote a GenreName book
+authorOfGenre(GenreName, AuthorName) :-
+    	genre(GID, GenreName),
+    	author(AID, AuthorName),
+    	book(_X,AID, GID, _Y).
+
+% Signature: longestBook(AuthorId, BookName)/2
+% Purpose: Check if BookName is the longest of AuthorId
+longestBook(AuthorId, BookName) :-
+	book(BookName, AuthorId, _X, Length),
+    not(longerBook(AuthorId, Length)).
+
+% Signature: longerBook(AuthorId, Length)/2
+% Purpose: Check if AuthorId wrote a longer then Length
+longerBook(AuthorId, Length) :-
+    book(_Y, AuthorId, _Z, CurrLength),
+    (CurrLength > Length).
+
+% Signature: versatileAuthor(AuthorName)/1
+% Purpose: Check if AuthorName has written books in at least three different genres
+versatileAuthor(AuthorName) :-
+    author(AuthorId, AuthorName),
+    book(_A, AuthorId, FirstGenre, _B),
+    book(_C, AuthorId, SecondGenre, _D),
+    book(_E, AuthorId, ThirdGenre, _F),
+    notEqual3(FirstGenre, SecondGenre, ThirdGenre).
+    
+% Signature: notEqual3(First, Second, Third)/3
+% Purpose: Check if 3 integers are not equal
+notEqual3(First, Second, Third) :-
+    (First =\= Second),
+    (Second =\= Third),
+    (Third =\= First).
+    	
